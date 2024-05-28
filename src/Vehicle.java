8b020ec3d2a5392eb0b5987a68b1977a3af8b813
ainugiri <giriprasa@gmail.com>
2024-05-28 14:24:42 +0530

    Create Vehicle.java

---
+++ src/Vehicle.java
@@ -0,0 +1,42 @@
+public class Vehicle {
+    // Instance variables
+    private String make;
+    private String model;
+    private int year;
+
+    // Constructor
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    // Getters and setters
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    // Other methods
+    // ...
+
+}
